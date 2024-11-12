86044a10fe4ff6c92761b52f43058e28730d61e4
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2024-11-12 15:22:02 +0100

    added seeds

---
+++ server/prisma/seed.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+  const adminRole = await prisma.role.create({
+    data: {
+      name: 'admin',
+    },
+  });
+
+  const userRole = await prisma.role.create({
+    data: {
+      name: 'user',
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { email: 'admin@example.com' },
+    update: {},
+    create: {
+      email: 'admin@example.com',
+      username: 'adminUser',
+      status: 'active',
+      password: 'secureAdminPassword',
+      roleId: adminRole.id,
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { email: 'user@example.com' },
+    update: {},
+    create: {
+      email: 'user@example.com',
+      username: 'regularUser',
+      status: 'active',
+      password: 'secureUserPassword',
+      roleId: userRole.id,
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
