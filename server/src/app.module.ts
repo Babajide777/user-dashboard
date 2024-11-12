9db6b77e853298c464cb95c43fdf142a18796d76
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2024-11-12 17:53:26 +0100

    created other folders

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule, RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/utils/guards/auth-guard.ts
@@ -0,0 +1,59 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { JWT_SECRET } from 'src/config/env';
+import { UsersQueryService } from 'src/users/users-query/users-query.service';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+    private readonly usersQueryService: UsersQueryService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException('Token not found');
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: JWT_SECRET as string,
+      });
+
+      const theUser = await this.usersQueryService.findUserByID(payload.id);
+
+      if (!theUser) {
+        throw new UnauthorizedException();
+      }
+      request['user'] = theUser.id;
+      request['business'] = theUser.businessId;
+      request['store'] = theUser.storeId;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
